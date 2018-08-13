508a644fc268e7c4a3392855a360029d6b3126d5
john <john@johng.cn>
2018-08-13 21:36:40 +0800

    init

---
+++ src/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "gitee.com/johng/gf/g"
+    "gitee.com/johng/gf/g/net/ghttp"
+)
+
+func init() {
+    g.View().AddPath("template")
+}
+
+func main() {
+    s := g.Server()
+    s.BindHandler("/", func(r *ghttp.Request) {
+        r.Response.Template("index.html")
+    })
+    s.SetPort(8199)
+    s.Run()
+}
